0dd71f4b6fe79bdc428ebb5a9f536355656e3f49
JinSo <kimjinso@qq.com>
2025-02-27 16:12:45 +0800

    feat: add code-editor

---
+++ src/components/code-editor/index.tsx
@@ -0,0 +1,74 @@
+import type { EditorProps, OnMount } from '@monaco-editor/react'
+import { Editor as MonacoEditor, loader } from '@monaco-editor/react'
+// import { useDebounceFn } from 'ahooks'
+import { type FC, useState } from 'react'
+import { libType } from './lib'
+
+loader.config({
+  paths: {
+    vs: '/monaco-editor/min/vs',
+  },
+})
+
+// 初始化配置
+const defaultOptions: EditorProps['options'] = {
+  folding: false,
+  lineNumbersMinChars: 3,
+  lineNumbers: 'on',
+  automaticLayout: true,
+  acceptSuggestionOnEnter: 'smart',
+  scrollbar: {
+    verticalScrollbarSize: 0,
+    verticalSliderSize: 4,
+    horizontal: 'hidden',
+    useShadows: false,
+  },
+  smoothScrolling: true,
+  minimap: {
+    enabled: false,
+  },
+  autoClosingBrackets: 'languageDefined',
+  autoClosingQuotes: 'languageDefined',
+  tabSize: 2,
+}
+
+export const CodeEditor: FC<EditorProps> = props => {
+  const [theme, setTheme] = useState('vs')
+
+  // 处理代码修改， args需要做一层透传来完善防抖，避免触发重复构建
+  // const { run: handleChange } = useDebounceFn<OnChange>(
+  //   (...args) => {
+  //     if (props.onChange) {
+  //       props.onChange(...args)
+  //     }
+  //   },
+  //   {
+  //     wait: 400,
+  //   },
+  // )
+
+  // 编辑器Mount钩子，需要注册一些事例
+  const onEditorMount: OnMount = (_editor, _monaco) => {
+    // 添加类型提示
+    _monaco.languages.typescript.javascriptDefaults.addExtraLib(libType, 'lib.d.ts')
+
+    if (props.onMount) {
+      props.onMount(_editor, _monaco)
+    }
+  }
+
+  return (
+    <MonacoEditor
+      loading={<span>loading</span>}
+      theme={theme}
+      {...props}
+      // onChange={handleChange}
+      onChange={props.onChange}
+      onMount={onEditorMount}
+      options={{
+        ...defaultOptions,
+        ...props.options,
+      }}
+    />
+  )
+}
---
+++ src/components/code-editor/lib.d.ts
@@ -0,0 +1,8 @@
+export const libType = `
+/**
+ * 用于自定义事件的预制参数提示
+ */
+declare const event: any
+
+declare const extraParam: any
+`
