e0e41215eb2e505aa611f3af9011fb19911fa192
JinSo <kimjinso@qq.com>
2025-03-14 10:15:54 +0800

    feat(contextMenu): cv

--- src/layouts/ContextMenu.tsx
+++ src/layouts/ContextMenu.tsx
@@ -9,12 +9,14 @@ import {
   ContextMenuSubTrigger,
   ContextMenuTrigger,
 } from '@/components/ui/context-menu'
+import { designer } from '@/editor'
 import {
   ArrowDown,
   ArrowUp,
   Clipboard,
   ClipboardCopy,
   ClipboardPaste,
+  ClipboardPen,
   Eye,
   Group,
   Layers,
@@ -33,6 +35,7 @@ interface MenuItem {
   children?: MenuItem[]
   separator?: boolean
   shortcut?: string
+  onClick?: () => void
 }
 
 const menuItems: MenuItem[] = [
@@ -85,6 +88,27 @@ const menuItems: MenuItem[] = [
     label: '粘贴',
     icon: ClipboardPaste,
     shortcut: '⌘V',
+  },
+  {
+    key: 'cv',
+    label: '拷贝',
+    icon: ClipboardPen,
+    onClick() {
+      const selected = designer.selection.getTopNodes()
+      if (selected.length === 0) return
+
+      for (const node of selected) {
+        if (node.isRoot) continue
+
+        const document = node.document
+        const cloneNodeSchema = node.export()
+        // 添加偏移
+        cloneNodeSchema.$dashboard!.rect!.x = (cloneNodeSchema.$dashboard!.rect!.x ?? 0) + 10
+        cloneNodeSchema.$dashboard!.rect!.y = (cloneNodeSchema.$dashboard!.rect!.y ?? 0) + 10
+        // 插入
+        document.insertNode(node.parent!, cloneNodeSchema, node.index + 1)
+      }
+    },
   },
   {
     key: 'copy-paste-as',
@@ -150,7 +174,7 @@ export const RendererContextMenu = ({ children }: RendererContextMenuProps) => {
                 <ContextMenuSubContent className='w-48 text-xs'>
                   {item.children.map(child => (
                     <Fragment key={child.key}>
-                      <ContextMenuItem className='h-8 px-2 text-xs gap-0'>
+                      <ContextMenuItem className='h-8 px-2 text-xs gap-0' onClick={child?.onClick}>
                         {child.icon && <child.icon className='w-4 h-4 mr-2' />}
                         {child.label}
                         {child.shortcut && (
@@ -163,7 +187,7 @@ export const RendererContextMenu = ({ children }: RendererContextMenuProps) => {
                 </ContextMenuSubContent>
               </ContextMenuSub>
             ) : (
-              <ContextMenuItem key={item.key} className='h-8 px-2 text-xs gap-0'>
+              <ContextMenuItem key={item.key} className='h-8 px-2 text-xs gap-0' onClick={item?.onClick}>
                 {item.icon && <item.icon className='w-4 h-4 mr-2' />}
                 {item.label}
                 {item.shortcut && <ContextMenuShortcut className='text-xs'>{item.shortcut}</ContextMenuShortcut>}
